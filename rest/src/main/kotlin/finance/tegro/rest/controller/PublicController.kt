4cf013494a1e1e294ef26a0800ec43a38fbe0da2
Anton F <imantonmeep@gmail.com>
2023-01-20 07:32:38 +0100

    Refactor public endpoints

--- rest/src/main/kotlin/finance/tegro/rest/controller/PublicController.kt
+++ rest/src/main/kotlin/finance/tegro/rest/controller/PublicController.kt
@@ -1,48 +1,55 @@
 package finance.tegro.rest.controller
 
 import finance.tegro.core.repository.ExchangePairRepository
-import finance.tegro.core.repository.ReserveRepository
 import finance.tegro.core.repository.SwapRepository
+import finance.tegro.core.toMsgAddress
 import finance.tegro.core.toSafeString
-import finance.tegro.rest.dto.PublicSummaryDTO
+import finance.tegro.rest.dto.PublicDexDTO
 import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.math.BigDecimal
 import java.time.Instant
 
 @RestController
 @RequestMapping("/public/v1")
 class PublicController(
     private val exchangePairRepository: ExchangePairRepository,
-    private val reserveRepository: ReserveRepository,
     private val swapRepository: SwapRepository,
 ) {
     @GetMapping("/dex")
-    fun getSummary(): Map<String, PublicSummaryDTO> =
+    fun getAllDexes(): Map<String, PublicDexDTO> =
         exchangePairRepository.findAll()
             .mapNotNull { exchangePair ->
                 try {
-                    requireNotNull(exchangePair.address.toSafeString()) to PublicSummaryDTO(
-                        url = "https://tegro.finance/swap?from=${exchangePair.token?.baseToken?.address?.toSafeString()}&to=${exchangePair.token?.quoteToken?.address?.toSafeString()}",
-                        baseId = requireNotNull(exchangePair.token?.baseToken?.address) { "Base token of ${exchangePair.address.toSafeString()} not found" },
-                        baseName = requireNotNull(exchangePair.token?.baseToken?.metadata?.name) { "Base token name of ${exchangePair.address.toSafeString()} not found" },
-                        baseSymbol = requireNotNull(exchangePair.token?.baseToken?.metadata?.symbol) { "Base token symbol of ${exchangePair.address.toSafeString()} not found" },
-                        baseVolume = swapRepository.findBaseVolume(exchangePair).orElse(BigDecimal.ZERO),
-                        quoteId = requireNotNull(exchangePair.token?.quoteToken?.address) { "Quote token of ${exchangePair.address.toSafeString()} not found" },
-                        quoteName = requireNotNull(exchangePair.token?.quoteToken?.metadata?.name) { "Quote token name of ${exchangePair.address.toSafeString()} not found" },
-                        quoteSymbol = requireNotNull(exchangePair.token?.quoteToken?.metadata?.symbol) { "Quote token symbol of ${exchangePair.address.toSafeString()} not found" },
-                        quoteVolume = swapRepository.findQuoteVolume(exchangePair).orElse(BigDecimal.ZERO),
-                        lastPrice = swapRepository.findPriceOn(exchangePair, Instant.now())
-                            .orElseThrow { throw IllegalArgumentException("Price of ${exchangePair.address.toSafeString()} now not found") },
-                    )
+                    getDex(requireNotNull(exchangePair.address.toSafeString()) { "Exchange pair ${exchangePair.address.toSafeString()} not vaid" })
                 } catch (e: Exception) {
-                    logger.warn(e) { "Failed to get summary for ${exchangePair.token?.baseToken?.metadata?.symbol}_${exchangePair.token?.quoteToken?.metadata?.symbol}" }
+                    logger.warn(e) { "Failed to get dex for ${exchangePair.address.toSafeString()}" }
                     null
                 }
             }
-            .associate { it }
+            .associate { it.id to it }
+
+    @GetMapping("/dex/{address}")
+    fun getDex(address: String): PublicDexDTO =
+        exchangePairRepository.findByAddress(address.toMsgAddress())
+            .map { exchangePair ->
+                PublicDexDTO(
+                    id = requireNotNull(exchangePair.address.toSafeString()) { "Address of ${exchangePair.address.toSafeString()} not valid" },
+                    url = "https://tegro.finance/swap?from=${exchangePair.token?.baseToken?.address?.toSafeString()}&to=${exchangePair.token?.quoteToken?.address?.toSafeString()}",
+                    baseId = exchangePair.token?.baseToken?.address?.toSafeString(),
+                    baseName = requireNotNull(exchangePair.token?.baseToken?.metadata?.name) { "Base token name of ${exchangePair.address.toSafeString()} not found" },
+                    baseSymbol = requireNotNull(exchangePair.token?.baseToken?.metadata?.symbol) { "Base token symbol of ${exchangePair.address.toSafeString()} not found" },
+                    baseVolume = swapRepository.findBaseVolume(exchangePair).orElse(null),
+                    quoteId = exchangePair.token?.quoteToken?.address?.toSafeString(),
+                    quoteName = requireNotNull(exchangePair.token?.quoteToken?.metadata?.name) { "Quote token name of ${exchangePair.address.toSafeString()} not found" },
+                    quoteSymbol = requireNotNull(exchangePair.token?.quoteToken?.metadata?.symbol) { "Quote token symbol of ${exchangePair.address.toSafeString()} not found" },
+                    quoteVolume = swapRepository.findQuoteVolume(exchangePair).orElse(null),
+                    lastPrice = swapRepository.findPriceOn(exchangePair, Instant.now()).orElse(null),
+                )
+            }
+            .orElseThrow { throw IllegalArgumentException("Exchange pair $address not found") }
+
 
     companion object : KLogging()
 }
--- rest/src/main/kotlin/finance/tegro/rest/dto/PublicDexDTO.kt
+++ rest/src/main/kotlin/finance/tegro/rest/dto/PublicDexDTO.kt
@@ -1,28 +1,29 @@
 package finance.tegro.rest.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import org.ton.block.MsgAddress
 import java.math.BigDecimal
 
-data class PublicSummaryDTO(
+data class PublicDexDTO(
+    @JsonProperty("id")
+    val id: String,
     @JsonProperty("url")
     val url: String,
     @JsonProperty("base_id")
-    val baseId: MsgAddress,
+    val baseId: String?,
     @JsonProperty("base_name")
     val baseName: String,
     @JsonProperty("base_symbol")
     val baseSymbol: String,
     @JsonProperty("base_volume")
-    val baseVolume: BigDecimal,
+    val baseVolume: BigDecimal?,
     @JsonProperty("quote_id")
-    val quoteId: MsgAddress,
+    val quoteId: String?,
     @JsonProperty("quote_name")
     val quoteName: String,
     @JsonProperty("quote_symbol")
     val quoteSymbol: String,
     @JsonProperty("quote_volume")
-    val quoteVolume: BigDecimal,
+    val quoteVolume: BigDecimal?,
     @JsonProperty("last_price")
-    val lastPrice: BigDecimal,
+    val lastPrice: BigDecimal?,
 )
