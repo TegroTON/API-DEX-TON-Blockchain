01bb547734ac10b35a577f720be8dbd83ae4d2ad
Andrey Pfau <andreypfau@ton.org>
2023-02-07 18:21:15 +0300

    bump ton-kotlin 0.2.14

--- ton-indexer/src/main/kotlin/finance/tegro/tonindexer/TonIndexer.kt
+++ ton-indexer/src/main/kotlin/finance/tegro/tonindexer/TonIndexer.kt
@@ -6,6 +6,8 @@ import finance.tegro.tonindexer.services.TonLiteApiService
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import org.ton.bitstring.Bits256
+import kotlin.streams.asStream
 
 suspend fun main(args: Array<String>): Unit = coroutineScope {
     TonLiteApiService.init()
@@ -17,15 +19,43 @@ suspend fun main(args: Array<String>): Unit = coroutineScope {
             val transactions =
                 (block.extra.value.inMsgDescr.value.nodes().count() + block.extra.value.outMsgDescr.value.nodes()
                     .count())
-            println("-1 block: ${block.info.value.seqNo} - transactions: $transactions")
+            block.extra.value.accountBlocks.value.nodes().forEach { (accountBlock, _) ->
+                println(" -1:${accountBlock.accountAddr}")
+                accountBlock.transactions.forEach { (transactionCell, _) ->
+                    println(
+                        "    ${
+                            Bits256(
+                                transactionCell.toCell().hash()
+                            )
+                        } - ${transactionCell.value.lt} [prev: ${transactionCell.value.prevTransLt}] - ${transactionCell.value.r1.value.inMsg.value?.value?.info?.javaClass?.simpleName}"
+                    )
+                }
+            }
         }
     }
     launch {
         BlockSubscriberService.blockFlow(0).collectLatest { block ->
-            val transactions =
+            val transactionsCount =
                 (block.extra.value.inMsgDescr.value.nodes().count() + block.extra.value.outMsgDescr.value.nodes()
                     .count())
-            println(" 0 block: ${block.info.value.seqNo} - transactions: $transactions")
+            println(" 0 block: ${block.info.value.seqNo} - transactions: $transactionsCount")
+            block.extra.value.accountBlocks.value.nodes().forEach { (accountBlock, _) ->
+                println("  0:${accountBlock.accountAddr}")
+                accountBlock.transactions.forEach { (transactionCell, _) ->
+                    println(
+                        "    ${
+                            Bits256(
+                                transactionCell.toCell().hash()
+                            )
+                        } - ${transactionCell.value.lt} - ${transactionCell.value.r1.value.inMsg.value?.value?.info?.javaClass?.simpleName}"
+                    )
+                }
+            }
         }
     }
+
+    val iterator = listOf(1).iterator()
+
+    iterator.asSequence().asStream()
+
 }
